4764750c13cf0bde50a467d10fa9e066f40cfa86
Lakshya223 <82098732+Lakshya223@users.noreply.github.com>
2022-11-28 00:48:59 +0530

    Update helper.js

--- source/voice-auth/helper.js
+++ source/voice-auth/helper.js
@@ -27,22 +27,8 @@ var thingsToRead = [
 	"India is an agricultural country. Most of the people live in villages and are farmers.\n They grow cereals, pulses, vegetables and fruits. The farmers lead a tough life. They get up early in the morning and go to the fields.\n They stay and work on the farm late till evening. The farmers usually live in kuchcha houses.\n Though, they work hard they remain poor. \nFarmers eat simple food; wear simple clothes and rear animals like cows, buffaloes and oxen. Without them there would be no cereals for us to eat. They play an important role in the growth and economy of a country.\n",
 ];
 
-/*// vanilla javascript queystring management
-var qs = (function(a) {
-    if (a == "") return {};
-    var b = {};
-    for (var i = 0; i < a.length; ++i)
-    {
-        var p=a[i].split('=', 2);
-        if (p.length == 1)
-            b[p[0]] = "";
-        else
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-    }
-    return b;
-})(window.location.search.substr(1).split('&'));*/
 
-// Get the Cognitive Services key from the querystring
+// Key and Endpoint By Microsoft Azure.
 var key = "9b82f23ff0804b6e8133ad9d903c3efe";
 var baseApi = "https://westus.api.cognitive.microsoft.com/";
 
